934aadffa54e5d42167f07f241d9ce34f1b66286
Sergey Chekanskiy <chekanskiy@gmail.com>
2020-07-10 16:12:54 +0200

    data added, download fixed

--- scripts/1_apple_download_report.py
+++ scripts/1_apple_download_report.py
@@ -7,10 +7,10 @@ sys.path.insert(0, APP_PATH)
 
 RELEASES_PATH = f"{APP_PATH}/../data-input/apple-mobility"
 
-api_revision = (datetime.datetime.now() - datetime.datetime(2020, 6, 22)).days
+api_revision = (datetime.datetime.now() - datetime.datetime(datetime.datetime.now().year, 7, 3)).days
 # 38 on 23 or May 2020
 
-link = f"https://covid19-static.cdn-apple.com/covid19-mobility-data/2011HotfixDev{api_revision}/v3/en-us/applemobilitytrends-"
+link = f"https://covid19-static.cdn-apple.com/covid19-mobility-data/2012HotfixDev{api_revision}/v3/en-us/applemobilitytrends-"
 
 
 def download_csv(date_report):
